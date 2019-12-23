68af624fe88318579967473ca265d8568e96d886
Chris Overgaauw <14801878+chrisovergaauw@users.noreply.github.com>
2019-12-23 11:46:54 +0100

    .

--- git/install.sh
+++ git/install.sh
@@ -3,8 +3,8 @@
 GIT_HOOK_BASE_URL=https://raw.githubusercontent.com/riddlesio/somatics-styleguide/experimental/githooks/git/
 
 install_githooks() {
-  curl GIT_HOOK_BASE_URL/git/$1/hooks/pre-commit > .git/hooks/pre-commit
-  curl GIT_HOOK_BASE_URL/git/$1/hooks/commit-msg > .git/hooks/commit-msg
+  curl $GIT_HOOK_BASE_URL/git/$1/hooks/pre-commit > .git/hooks/pre-commit
+  curl $GIT_HOOK_BASE_URL/git/$1/hooks/commit-msg > .git/hooks/commit-msg
   rm -rf ".git/hooks/*.sample"
   chmod 755 ".git/hooks/*"
 }
