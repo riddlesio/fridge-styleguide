2fb972b460f359cd2755e54c86fb95546d0aec32
Chris Overgaauw <14801878+chrisovergaauw@users.noreply.github.com>
2019-12-23 11:37:46 +0100

    added simple script. check if it works

---
+++ git/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BASE_URL=https://raw.githubusercontent.com/riddlesio/somatics-styleguide/master/
+
+install_githooks() {
+  curl $BASE_URL/git/$1/hooks/pre-commit > .git/hooks/pre-commit
+  curl $BASE_URL/git/$1/hooks/commit-msg > .git/hooks/commit-msg
+  rm -rf ".git/hooks/*.sample"
+  chmod 755 ".git/hooks/*"
+}
+
+install_githooks $@
