38131a19d34278525e7e8efa134c76b03b8519a3
Chris Overgaauw <14801878+chrisovergaauw@users.noreply.github.com>
2019-12-23 12:13:50 +0100

    oops

--- git/install.sh
+++ git/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-GIT_HOOK_BASE_URL="https://raw.githubusercontent.com/riddlesio/somatics-styleguide/master/git/"
+GIT_HOOK_BASE_URL="https://raw.githubusercontent.com/riddlesio/somatics-styleguide/master/git"
 
 install_githooks() {
-  curl -m 2 -s $GIT_HOOK_BASE_URL/git/$1/hooks/pre-commit > /tmp/pre-commit && mv /tmp/pre-commit .git/hooks/pre-commit
-  curl -m 2 -s $GIT_HOOK_BASE_URL/git/$1/hooks/commit-msg > /tmp/commit-msg && mv /tmp/commit-msg .git/hooks/commit-msg
+  curl -m 2 -s $GIT_HOOK_BASE_URL/$1/hooks/pre-commit > /tmp/pre-commit && mv /tmp/pre-commit .git/hooks/pre-commit
+  curl -m 2 -s $GIT_HOOK_BASE_URL/$1/hooks/commit-msg > /tmp/commit-msg && mv /tmp/commit-msg .git/hooks/commit-msg
   rm -rf .git/hooks/*.sample >> /dev/null 2>&1
   rm -rf /tmp/pre-commit >> /dev/null 2>&1
   rm -rf /tmp/commit-msg >> /dev/null 2>&1
